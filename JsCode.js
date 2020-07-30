35ecc23fb5ae05d6a00fa02f3a101364960513a5
Family <matmuratov.jamshid@gmailcom>
2020-07-30 17:09:39 +0500

    Это наш первый коммент

---
+++ JsCode.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var number = 5;
+var string = "Hello";
+var sum = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['orange', 'apple', 'banana'];
+
+console.log(arr[0]);
+
+//alert("Hello world!");
+//let answer = confirm("Are you here");
+
+//console.log(answer);
+let answer = prompt("Есть ли вам 34 лет");
+
+console.log(answer)
+
